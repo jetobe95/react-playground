be0b48102f9a807696b168547e66b4c50beed280
Jeffrey Torres <jtorres@technisupport.com>
2021-03-15 13:29:43 -0500

    contador

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Subject, interval, merge, empty, NEVER } from 'rxjs'
+import { debounceTime, map, mapTo, scan, startWith, switchMap, tap } from 'rxjs/operators'
+
+const subjectPlay = new Subject().pipe(mapTo({ count: true }))
+const subjectPause = new Subject().pipe(mapTo({ count: false }))
+const subjectSpeed = new Subject().pipe(
+  map((speed) => ({ speed: +speed })),
+)
+
+const subjectIncrease = new Subject().pipe(
+  map((increase) => ({ increase: +increase })),
+)
+
+const clock = interval(1000).pipe(mapTo(-1))
+
+
+
 
 function App() {
+  const [state, setState] = useState({})
+
+
+
+  useEffect(() => {
+
+    const counter = merge(subjectPlay, subjectPause, subjectSpeed,subjectIncrease)
+      .pipe(
+        startWith({
+          value: 0,
+          increase: 1,
+          speed: 1000,
+          count: false,
+          countup: true
+        }),
+        tap(_ => setState((oldState) => ({ ...oldState, ...state }))),
+        scan((state, curr) => ({ ...state, ...curr }), {}),
+        switchMap(state => state.count ? interval(state.speed).pipe(
+          tap(_ => state.value += state.countup ? state.increase : -state.increase),
+          tap(_ => setState((oldState) => ({ ...oldState, ...state })))
+        ) : NEVER),
+      )
+
+    counter.subscribe(console.log)
+
+
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>RxJS</h1>
+      <h1>{state?.value}</h1>
+      <input type="number" onChange={({ target: { value } }) => subjectSpeed.next(value)} />
+      <input type="number" onChange={({ target: { value } }) => subjectIncrease.next(value)} />
+      <button onClick={() => subjectPlay.next()}>{!state?.count ?  "EMPEZAR":"CONTINUAR"}</button>
+      <button onClick={() => subjectPause.next()}>PAUSAR</button>
+      <br/>
+      <code>{JSON.stringify(state,null,4)}</code>
     </div>
   );
 }
