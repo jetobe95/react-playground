db38b13ab8b0ff6175742837bcc708a1154aa6e5
Jeffrey Torres <jtorres@technisupport.com>
2021-04-19 22:40:12 -0500

    actualizacion de navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Aplicación de React-Redux</a>
+                <a className="navbar-brand" href="/">Aplicación de React-Redux +  Redux Observables </a>
             </div>
         </nav>
     )
